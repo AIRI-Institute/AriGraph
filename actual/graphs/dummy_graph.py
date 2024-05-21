f56c8b8890f0c2065e501cfae6d4f422cc8942f7
n.semenov <n.semenov@an01.zhores>
2024-05-21 17:53:43 +0300

    final code

--- actual/graphs/dummy_graph.py
+++
@@ -1,8 +0,0 @@
-from parent_graph import TripletGraph
-
-class DummyGraph(TripletGraph):
-    def __init__(self, model, system_prompt):
-        super().__init__(model, system_prompt)
-        
-    def update(self):
-        return []
