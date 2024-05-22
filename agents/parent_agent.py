b57971347631a9c0b15757a8195fdf68a9035bda
n.semenov <n.semenov@an01.zhores>
2024-05-22 21:54:43 +0300

    final

--- agents/parent_agent.py
+++ agents/parent_agent.py
@@ -1,12 +1,7 @@
 import ast
-import requests
 from time import sleep
 from openai import OpenAI
 
-# FOR REMOVE WITH REQUESTS!!!
-VPS_IP = "146.0.73.157"
-port = 8000
-API_KEY = "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"
 
 class GPTagent:
     def __init__(self, model, system_prompt, api_key):
@@ -17,62 +12,46 @@ class GPTagent:
             api_key=api_key,
         )
 
-    # def generate(self, prompt, jsn = False, t = 0.7):
-    #     if jsn:   
-    #         chat_completion = self.client.chat.completions.create(
-    #             messages=[
-    #                 {
-    #                     "role": "system",
-    #                     "content": self.system_prompt,
-    #                 },
-    #                 {
-    #                     "role": "user",
-    #                     "content": prompt,
-    #                 }
-    #             ],
-    #             model=self.model,
-    #             response_format={"type": "json_object"},
-    #             temperature=t
-    #         )
-    #     else:
-    #         chat_completion = self.client.chat.completions.create(
-    #             messages=[
-    #                 {
-    #                     "role": "system",
-    #                     "content": self.system_prompt,
-    #                 },
-    #                 {
-    #                     "role": "user",
-    #                     "content": prompt,
-    #                 }
-    #             ],
-    #             model=self.model,
-    #             temperature=t
-    #         )
-    #     response = chat_completion.choices[0].message.content
-    #     prompt_tokens = chat_completion.usage.prompt_tokens
-    #     completion_tokens = chat_completion.usage.completion_tokens
+    def generate(self, prompt, jsn = False, t = 0.7):
+        if jsn:   
+            chat_completion = self.client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": self.system_prompt,
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    }
+                ],
+                model=self.model,
+                response_format={"type": "json_object"},
+                temperature=t
+            )
+        else:
+            chat_completion = self.client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": self.system_prompt,
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    }
+                ],
+                model=self.model,
+                temperature=t
+            )
+        response = chat_completion.choices[0].message.content
+        prompt_tokens = chat_completion.usage.prompt_tokens
+        completion_tokens = chat_completion.usage.completion_tokens
 
-    #     cost = completion_tokens * 3 / 100000 + prompt_tokens * 1 / 100000
-        # self.total_amount += cost
-    #     return response, cost
-        
-
-    # FOR REMOVE!!!    
-    def generate(self, prompt, t = 1, jsn = False):
-        messages = [{"role": "system", "content": self.system_prompt},
-                    {"role": "user", "content": prompt}]
-
-        response = requests.post(
-            f"http://{VPS_IP}:{port}/openai_api",
-            json={"api_key": API_KEY, "messages": messages, "model_type": self.model, "temperature": t, "jsn": jsn}
-        )
-        resp = response.json()["response"]
-        usage = response.json()["usage"]
-        cost = usage["completion_tokens"] * 3 / 100000 + usage["prompt_tokens"] * 1 / 100000
+        cost = completion_tokens * 3 / 100000 + prompt_tokens * 1 / 100000
         self.total_amount += cost
-        sleep(1)
-        return resp, cost      
+        return response, cost
+           
 
     
     def item_processing_scores(self, observation, plan):
